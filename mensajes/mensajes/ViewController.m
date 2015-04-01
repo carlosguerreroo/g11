cd59c8b948dc28d963c8fc5a25c3c76979c0b2b2
Carlos Omana <araogaraog@gmail.com>
2015-03-31 23:06:25 -0600

    Adds login and sign up view

--- mensajes/mensajes/ViewController.m
+++ mensajes/mensajes/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *signup;
+@property (weak, nonatomic) IBOutlet UIView *login;
 
 @end
 
@@ -16,12 +18,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+- (IBAction)changeView:(UISegmentedControl *)sender {
+
+
+
 }
 
 @end
