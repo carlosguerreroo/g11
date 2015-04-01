8c6a3e8db284b0bc6803fc71ea4fd1b1bf386794
Carlos Omana <araogaraog@gmail.com>
2015-03-31 22:58:06 -0600

    Adds objective C project

---
+++ mensajes/mensajes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mensajes
+//
+//  Created by Carlos Guerrero on 3/31/15.
+//  Copyright (c) 2015 grupoonce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mensajes/mensajes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mensajes
+//
+//  Created by Carlos Guerrero on 3/31/15.
+//  Copyright (c) 2015 grupoonce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
