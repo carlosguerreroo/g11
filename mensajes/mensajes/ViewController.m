57a01f19aaa9585374803303de7351924a97e444
Carlos Omana <araogaraog@gmail.com>
2015-03-31 23:21:20 -0600

    Hide and Show views

--- mensajes/mensajes/ViewController.m
+++ mensajes/mensajes/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UIView *signup;
 @property (weak, nonatomic) IBOutlet UIView *login;
 
@@ -18,15 +19,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-}
+    [self signup].hidden = NO;
+    [self login].hidden = YES;}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 - (IBAction)changeView:(UISegmentedControl *)sender {
 
-
-
+    switch (sender.selectedSegmentIndex)
+    {
+        case 0:
+            [self signup].hidden = NO;
+            [self login].hidden = YES;
+            break;
+        case 1:
+            [self signup].hidden = YES;
+            [self login].hidden = NO;
+            break;
+        default: 
+            break; 
+    }
 }
 
 @end
