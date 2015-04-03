69e2b1ff7b2d8e8e50d2aeb7b71b8d7c49bb868d
Carlos Omana <araogaraog@gmail.com>
2015-04-02 19:30:33 -0600

    Adds cities picket implementation

--- mensajes/mensajes/ViewController.m
+++ mensajes/mensajes/ViewController.m
@@ -8,19 +8,28 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    
+    NSArray *_pickerData;
+}
 
 @property (weak, nonatomic) IBOutlet UIView *signup;
 @property (weak, nonatomic) IBOutlet UIView *login;
+@property (weak, nonatomic) IBOutlet UIPickerView *cities;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     [self signup].hidden = NO;
-    [self login].hidden = YES;}
+    [self login].hidden = YES;
+    _pickerData = @[@"Item 1", @"Item 2", @"Item 3", @"Item 4", @"Item 5", @"Item 6"];
+    self.cities.dataSource = self;
+    self.cities.delegate = self;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -41,5 +50,25 @@
             break; 
     }
 }
+
+
+// The number of columns of data
+- (int)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+// The number of rows of data
+- (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return _pickerData.count;
+}
+
+// The data to return for the row and component (column) that's being passed in
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return _pickerData[row];
+}
+
 
 @end
