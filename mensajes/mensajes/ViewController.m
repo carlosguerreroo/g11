2243d501cbdff32b56c26fb81d6e9ebf6914b2cc
Carlos Omana <araogaraog@gmail.com>
2015-04-02 21:10:56 -0600

    Adds border color and border radio to text fields

--- mensajes/mensajes/ViewController.m
+++ mensajes/mensajes/ViewController.m
@@ -11,11 +11,22 @@
 @interface ViewController () {
     
     NSArray *_pickerData;
+    UIColor *grayColor;
+    UIColor *yellowColor;
 }
 
 @property (weak, nonatomic) IBOutlet UIView *signup;
 @property (weak, nonatomic) IBOutlet UIView *login;
+@property (weak, nonatomic) IBOutlet UIView *header;
+@property (weak, nonatomic) IBOutlet UIView *selector;
 @property (weak, nonatomic) IBOutlet UIPickerView *cities;
+
+//Login items
+
+
+
+//Signup items
+@property (strong, nonatomic) IBOutletCollection(UITextField) NSArray *signupItems;
 
 @end
 
@@ -27,10 +38,26 @@
     [self signup].hidden = NO;
     [self login].hidden = YES;
     _pickerData = @[@"Item 1", @"Item 2", @"Item 3", @"Item 4", @"Item 5", @"Item 6"];
+    grayColor = [UIColor colorWithRed:0.651 green:0.651 blue:0.651 alpha:1];
+    yellowColor = [UIColor colorWithRed:0.996 green:0.761 blue:0.133 alpha:1];
+
     self.cities.dataSource = self;
     self.cities.delegate = self;
+    
+    //Setting background color
+    self.signup.backgroundColor = grayColor;
+    self.login.backgroundColor = grayColor;
+    self.header.backgroundColor = grayColor;
+    self.selector.backgroundColor = grayColor;
+    
+    
+    for (UITextField *object in self.signupItems) {
+        object.layer.cornerRadius = 4.0f;
+        object.layer.masksToBounds= YES;
+        object.layer.borderColor = [yellowColor CGColor];
+        object.layer.borderWidth = 1.5f;
+	}
 }
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
@@ -39,12 +66,12 @@
     switch (sender.selectedSegmentIndex)
     {
         case 0:
-            [self signup].hidden = NO;
-            [self login].hidden = YES;
+            self.signup.hidden = NO;
+            self.login.hidden = YES;
             break;
         case 1:
-            [self signup].hidden = YES;
-            [self login].hidden = NO;
+            self.signup.hidden = YES;
+            self.login.hidden = NO;
             break;
         default: 
             break; 
