cbddae9d782407c7980f07e4f911f43050102271
Carlos Omana <araogaraog@gmail.com>
2015-04-03 18:57:35 -0600

    Adds send a message controller

---
+++ mensajes/mensajes/SendMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SendMessageViewController.h
+//  mensajes
+//
+//  Created by Carlos Guerrero on 4/3/15.
+//  Copyright (c) 2015 grupoonce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendMessageViewController : UIViewController
+
+@end
