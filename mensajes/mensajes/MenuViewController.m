68eea540a5119d71e8b3dbcf6b23fa72451f45f4
Carlos Omana <araogaraog@gmail.com>
2015-04-03 19:09:25 -0600

    Adds border and color to menu buttons

--- mensajes/mensajes/MenuViewController.m
+++ mensajes/mensajes/MenuViewController.m
@@ -8,7 +8,15 @@
 
 #import "MenuViewController.h"
 
-@interface MenuViewController ()
+@interface MenuViewController () {
+
+    UIColor *grayColor;
+    UIColor *yellowColor;
+
+
+}
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *optionsButtons;
+@property (weak, nonatomic) IBOutlet UIButton *logoutButton;
 
 @end
 
@@ -16,6 +24,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    yellowColor = [UIColor colorWithRed:0.996 green:0.761 blue:0.133 alpha:1];
+
+    
+    for (UITextField *object in self.optionsButtons) {
+        object.layer.cornerRadius = 4.0f;
+        object.layer.masksToBounds= YES;
+        object.layer.borderColor = [yellowColor CGColor];
+        object.layer.borderWidth = 1.5f;
+        object.backgroundColor = yellowColor;
+    }
+    _logoutButton.layer.cornerRadius = 4.0f;
+    _logoutButton.layer.masksToBounds= YES;
+    _logoutButton.layer.borderWidth = 1.5f;
 }
 
 - (void)didReceiveMemoryWarning {
