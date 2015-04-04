5f7791791eb869d9dfc73bf76d6e90ce96b4c160
Carlos Omana <araogaraog@gmail.com>
2015-04-03 18:55:34 -0600

    Adds questions controller

---
+++ mensajes/mensajes/QuestionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionsViewController.h
+//  mensajes
+//
+//  Created by Carlos Guerrero on 4/3/15.
+//  Copyright (c) 2015 grupoonce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionsViewController : UIViewController
+
+@end
