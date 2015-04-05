6d513d1b9ea17473a2fc61932db6674284dc4efb
Carlos Omana <araogaraog@gmail.com>
2015-04-05 13:47:29 -0500

    Adds chat view controller

---
+++ mensajes/mensajes/ChatViewController.m
@@ -0,0 +1,31 @@
+//
+//  ChatViewController.m
+//  mensajes
+//
+//  Created by Carlos Guerrero on 4/5/15.
+//  Copyright (c) 2015 grupoonce. All rights reserved.
+//
+
+#import "ChatViewController.h"
+
+@interface ChatViewController ()
+
+@end
+
+@implementation ChatViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+@end
