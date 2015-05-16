576394e2e181d68f99a738df2a54af01272b7b08
Carlos Omana <araogaraog@gmail.com>
2015-05-16 16:43:22 -0500

    Adds Nav bar to admin view

---
+++ mensajes/mensajes/AdminNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdminNavViewController.h
+//  mensajes
+//
+//  Created by Carlos Guerrero on 5/16/15.
+//  Copyright (c) 2015 grupoonce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdminNavViewController : UINavigationController
+
+@end
---
+++ mensajes/mensajes/AdminNavViewController.m
@@ -0,0 +1,37 @@
+//
+//  AdminNavViewController.m
+//  mensajes
+//
+//  Created by Carlos Guerrero on 5/16/15.
+//  Copyright (c) 2015 grupoonce. All rights reserved.
+//
+
+#import "AdminNavViewController.h"
+
+@interface AdminNavViewController ()
+
+@end
+
+@implementation AdminNavViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
